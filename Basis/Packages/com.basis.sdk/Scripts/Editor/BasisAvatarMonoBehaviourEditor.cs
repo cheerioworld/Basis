d9afd6cd93891d5b7cede1a2960250bbe690fe6d
Solaris Nite <solaris@cheerioworld.com>
2026-02-23 14:34:14 -0800

    Disabled compilation of BasisAvatarMonoBehaviourEditor because it depends on BasisAvatarMonoBehaviour which would expand dependency scope to the basis framework (we only want the avatar builder SDK)

--- Basis/Packages/com.basis.sdk/Scripts/Editor/BasisAvatarMonoBehaviourEditor.cs
+++ Basis/Packages/com.basis.sdk/Scripts/Editor/BasisAvatarMonoBehaviourEditor.cs
@@ -1,3 +1,4 @@
+#if false
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine.UIElements;
@@ -52,3 +53,4 @@ public class BasisAvatarMonoBehaviourEditor : Editor
         root.Add(messageField);
     }
 }
+#endif
